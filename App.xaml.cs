5fc2d209e5f5301b2a4e04ccf574ad1e233bfbb1
mat3o <matlagit@outlook.com>
2023-01-06 16:00:09 +0100

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace testOdataMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace testOdataMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
