7883a0f059d0a20badfb8b9ac7b7266bd053d8cb
mat3o <matlagit@outlook.com>
2023-01-22 02:36:25 +0100

    add mac catalyst example

---
+++ odataMacCatalystExample/Main.cs
@@ -0,0 +1,21 @@
+﻿using odataMacCatalystExample;
+using testOdataMaui;
+
+ODataServiceContext odataContext;
+
+odataContext = new(new Uri("http://services.odata.org/TripPinRESTierService/(S(3mslpb2bc0k5ufk24olpghzx))/"));
+
+try
+{
+    var result = odataContext.People.Execute();
+}
+catch (Exception ex)
+{
+    throw;
+}
+
+// This is the main entry point of the application.
+// If you want to use a different Application Delegate class from "AppDelegate"
+// you can specify it here.
+UIApplication.Main (args, null, typeof (AppDelegate));
+
